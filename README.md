495f86198e5e17af00dfbfe3c39fe393a26e631a
Carlos Martinez <edu6jas@gmail.com>
2023-11-04 23:59:38 -0400

    backend 55% ready

--- README.md
+++ README.md
@@ -2,17 +2,51 @@
 
 nodejs app
 
-### Before run
+## Before run
 install the database (recipes_app.sql) and run this command on folder root
 `npm install`
 
 ### How to run
 `node index.js`
 
-### libraries used
+## libraries used
 - express.js
 - bcryptjs (encrypt passwords),
 - cors (CORS middleware)
 - mysql2,
 - sequelize (ORM),
 - uuid (random id generator)
+
+## API endpoints
+All routes starts with `<<host>>/api`
+### users
+## `/api/users/register` (POST)
+
+- params
+```
+{ 
+    email: <string, required>, 
+    password: <string, required>, 
+    firstName: <string, required>, 
+    lastName: <string, optional> 
+}
+```
+- returns user id or an specific message in case didn't pass validation
+
+## `/api/users/login` (POST)
+
+- params
+```
+{ 
+    email: <string, required>, 
+    password: <string, required> 
+}
+```
+- returns user basic info or null
+
+### recipes
+## `/api/recipes/getRecipe/:id` (GET)
+
+- params
+`recipe id`
+- returns recipe
--- services/userServices.js
+++ services/userServices.js
@@ -7,24 +7,36 @@ async function registerUser(body) {
     const Users = getUsersModel();
     const { email, password, firstName, lastName } = body;
 
-    const alreadyExist = await Users.findOne({ where: { email } });
-    if(alreadyExist){
+    try {
+         // simple validations
+        if(!/[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}/.test(email)){
+            return "Invalid email";
+        }
+        if(password.length < 6){
+            return "Password needs at least 6 characters";
+        }
+        const alreadyExist = await Users.findOne({ where: { email } });
+        if(alreadyExist){
+            return "Email already in use";
+        }
+        // encrypt password
+        const salt = await bcrypt.genSalt(10);
+        const encryptedPass = await bcrypt.hash(password, salt);
+    
+        const newUser = new Users({
+            id: uuidv4(),
+            email,
+            firstName, 
+            lastName,
+            password: encryptedPass
+        })
+        await newUser.save();
+    
+        return newUser.id;
+    } catch (err) {
+        console.log(err);
         return null;
     }
-    
-    const salt = await bcrypt.genSalt(10);
-    const encryptedPass = await bcrypt.hash(password, salt);
-
-    const newUser = new Users({
-        id: uuidv4(),
-        email,
-        firstName, 
-        lastName,
-        password: encryptedPass
-    })
-    await newUser.save();
-
-    return newUser.id;
 }
 
 
@@ -33,13 +45,18 @@ async function loginUser({ email, password }) {
     const storedUser = await Users.findOne({ where: { email } });
 
     if(!storedUser){
-        return false;
+        return null;
     }
 
     const encryptedPass = storedUser.get('password')
     const validPass = await bcrypt.compare(password, encryptedPass);
 
-    return validPass ? true : false;
+    if(!validPass) return null;
+
+    return {
+        email: storedUser.email,
+        firstName: storedUser.firstName
+    }
 }
 
 

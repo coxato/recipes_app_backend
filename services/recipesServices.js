fb0d216a231b15d49d9bc7bd04104efeb45125eb
Carlos Martinez <edu6jas@gmail.com>
2023-11-05 23:34:22 -0400

    refactor

--- services/recipesServices.js
+++ services/recipesServices.js
@@ -1,6 +1,4 @@
 const { getRecipesModel } = require("../models/recipes");
-const { getIngredientModel } = require("../models/ingredients");
-const { getRecipesIngredientsModel } = require("../models/recipe_ingredients");
 const { v4: uuidv4 } = require('uuid');
 const { getDBInstance } = require("../db");
 const { Sequelize } = require("sequelize");
@@ -22,21 +20,40 @@ async function createRecipe(body) {
     return newRecipe.id;
 }
 
+// set ingredients array for each recipe 
+async function _setIngredientsArray(recipesArr) {
+    const dbInstance = getDBInstance();
+
+    for (const recipe of recipesArr) {
+        const ingredientsForThisRecipe = await dbInstance.query(`
+            SELECT ingredients.name, ingredients.id  FROM ingredients 
+            INNER JOIN recipe_ingredients ON ingredients.id = recipe_ingredients.id_ingredient 
+            WHERE recipe_ingredients.id_recipe = ${recipe.id}
+        `,
+        { type: Sequelize.QueryTypes.SELECT }
+        );
+        // declare array
+        recipe.ingredients = ingredientsForThisRecipe;
+    }
+}
 
 async function getRecipe({ id }) {
     const Recipes = getRecipesModel();
     const recipe = await Recipes.findOne({ where: { id } });
+    if(!recipe) return null;
+
+    await _setIngredientsArray([recipe]);
+    console.log("@@@");
+    console.log(recipe.ingredients);
 
     return {
-        recipe
+        ...recipe.dataValues,
+        ingredients: recipe.ingredients
     }
 }
 
-
 async function getRecipesByIngredients({ ingredients }) {
     const dbInstance = getDBInstance();
-    const Ingredients = getIngredientModel();
-    const IngredientsRecipes = getRecipesIngredientsModel();
 
     function buildJoins(arr) {
         let r = "";
@@ -61,17 +78,7 @@ async function getRecipesByIngredients({ ingredients }) {
 
     // add array of ingredients for each recipe found
     if(results.length){
-        for (const recipe of results) {
-            const ingredientsForThisRecipe = await dbInstance.query(`
-                SELECT ingredients.name, ingredients.id  FROM ingredients 
-                INNER JOIN recipe_ingredients ON ingredients.id = recipe_ingredients.id_ingredient 
-                WHERE recipe_ingredients.id_recipe = ${recipe.id}
-            `,
-            { type: Sequelize.QueryTypes.SELECT }
-            );
-            // declare array
-            recipe.ingredients = ingredientsForThisRecipe;
-        }
+        await _setIngredientsArray(results);
     }
 
     return {
